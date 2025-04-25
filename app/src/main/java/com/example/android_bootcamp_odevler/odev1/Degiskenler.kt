54737bf7c0eea2ddc727668f9a59208524b9aaab
furkanerdogan <furkanerdd1@gmail.com>
2025-04-25 15:08:04 +0300

    1.Ödev

---
+++ app/src/main/java/com/example/android_bootcamp_odevler/odev1/Degiskenler.kt
@@ -0,0 +1,103 @@
+package com.example.android_bootcamp_odevler.odev1
+
+fun main(){
+    val sehir: String = "Ankara"
+    val ulke: String = "Türkiye"
+    val telefon: String = "+90 555 555 55 55"
+    val postaKodu: String = "06352"
+    val email: String = "furkanerdd1@gmail.com"
+    val meslek: String = "Ögrenci"
+    val stokMiktari: Int = 100
+    val musteriAdi: String = "Furkan"
+    val bakiye: Double = 1520.75
+    val dogumGunu: String = "2000-01-01"
+    val maas: Double = 5000.0
+    val medeniDurum: String = "Bekar"
+    val urunYorum: String = "Çok iyi"
+
+    val odemeTarihi: String = "2025-04-20"
+    val odeme: Double = 299.99
+    val siparisAdeti: Int = 3
+    val arabaModeli: String = "BMW 320i"
+    val kitapAdi: String = "Kotlin 101"
+    val yayinlamaTarihi: String = "2024-09-10"
+    val indirimMiktari: Int = 20
+    val odaSayisi: Int = 2
+    val enlem: Double = 39.9208
+    val boylam: Double = 32.8541
+    val urunAdi: String = "Kablosuz Kulaklık"
+    val yemekFiyati: Double = 150.0
+
+    val marka: String = "Samsung"
+    val muzikAdi: String = "Yolcu"
+    val videoSuresi: String = "3:45"
+    val urunPuani: Double = 4.7
+    val resimAdi: String = "manzara.jpg"
+    val dosyaFormati: String = "jpg"
+    val renk: String = "Kırmızı"
+    val renkKodu: String = "#FF0000"
+    val telefonModeli: String = "iPhone 13"
+    val ekranBoyutu: Double = 6.1
+    val agirlik: Double = 180.5
+
+    val ulusalGun: String = "29 Ekim"
+    val tatilGunu: String = "1 Mayıs"
+    val rezervasyonTarihi: String = "2025-08-01"
+    val sokakAdi: String = "Atatürk Caddesi"
+    val otobusHatti: String = "528"
+    val kalanDakika: Int = 5
+    val takipKodu: String = "TRK123456789"
+    val kuponSuresi: String = "2 gün"
+    val kuponKodu: String = "YAZ25"
+    val faturaAdresi: String = "Çankaya,Ankara"
+
+    println("Şehir: $sehir")
+    println("Ülke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("Email: $email")
+    println("Meslek: $meslek")
+    println("Stok Miktarı: $stokMiktari")
+    println("Müşteri Adı: $musteriAdi")
+    println("Bakiye: $bakiye")
+    println("Doğum Günü: $dogumGunu")
+    println("Maaş: $maas")
+    println("Medeni Durum: $medeniDurum")
+    println("Ürün Yorum: $urunYorum")
+
+    println("Ödeme Tarihi: $odemeTarihi")
+    println("Ödeme: $odeme")
+    println("Sipariş Adeti: $siparisAdeti")
+    println("Araba Modeli: $arabaModeli")
+    println("Kitap Adı: $kitapAdi")
+    println("Yayınlama Tarihi: $yayinlamaTarihi")
+    println("İndirim Miktarı: $indirimMiktari")
+    println("Oda Sayısı: $odaSayisi")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+    println("Ürün Adı: $urunAdi")
+    println("Yemek Fiyatı: $yemekFiyati")
+
+    println("Marka: $marka")
+    println("Müzik Adı: $muzikAdi")
+    println("Video Süresi: $videoSuresi")
+    println("Ürün Puanı: $urunPuani")
+    println("Resim Adı: $resimAdi")
+    println("Dosya Formatı: $dosyaFormati")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Telefon Modeli: $telefonModeli")
+    println("Ekran Boyutu: $ekranBoyutu")
+    println("Ağırlık: $agirlik")
+
+    println("Ulusal Gün: $ulusalGun")
+    println("Tatil Günü: $tatilGunu")
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    println("Sokak Adı: $sokakAdi")
+    println("Otobüs Hattı: $otobusHatti")
+    println("Kalan Dakika: $kalanDakika")
+    println("Takip Kodu: $takipKodu")
+    println("Kupon Süresi: $kuponSuresi")
+    println("Kupon Kodu: $kuponKodu")
+    println("Fatura Adresi: $faturaAdresi")
+}
